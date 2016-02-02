62ef9832c29bb6a64cc92a78ccbd79769ace1194
okay <okay@chert>
2016-02-01 17:51:57 -0800

    add uptime fake data script

---
+++ scripts/fakedata/host_generator.py
@@ -0,0 +1,35 @@
+import json
+import random
+import sys
+
+HOSTS = [ "www.facebook.com", "www.yahoo.com", "www.google.com", "www.reddit.com", "github.com" ]
+STATII = [200, 403, 404, 500, 503]
+
+NUM_RECORDS = 10000
+if len(sys.argv) > 1:
+    NUM_RECORDS = int(sys.argv[1])
+
+
+def rand_record():
+    record = {
+        "ints" : {},
+        "strs" : {}
+    }
+
+    record["strs"]["status"] = str(random.choice(STATII))
+    record["strs"]["host"] = random.choice(HOSTS)
+    record["ints"]["ping"] = abs(random.gauss(60, 20))
+
+    return record
+
+def generate_records(n):
+    records = []
+    for j in xrange(n):
+        record = rand_record()
+        records.append(record)
+        print json.dumps(record)
+
+
+    return records
+
+generate_records(NUM_RECORDS)
