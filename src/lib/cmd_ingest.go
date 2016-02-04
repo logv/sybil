5962088a2213fc145067dde2f36245c17493c104
okay <okay@chert>
2016-02-03 19:49:42 -0800

    allowing for nested json ingestion of records

--- src/lib/cmd_ingest.go
+++ src/lib/cmd_ingest.go
@@ -9,6 +9,28 @@ import (
 	"os"
 )
 
+type Dictionary map[string]interface{}
+
+func ingest_dictionary(r *Record, recordmap *Dictionary, prefix string) {
+	for k, v := range *recordmap {
+		key_name := fmt.Sprintf("%s%s", prefix, k)
+		prefix_name := fmt.Sprintf("%s.", key_name)
+		switch iv := v.(type) {
+		case string:
+			fmt.Println("ADDING FIELD", key_name, iv)
+			r.AddStrField(key_name, iv)
+		case float64:
+			fmt.Println("ADDING FIELD", key_name, iv)
+			r.AddIntField(key_name, int(iv))
+		case map[string]interface{}:
+			d := Dictionary(iv)
+			ingest_dictionary(r, &d, prefix_name) 
+		case Dictionary:
+			ingest_dictionary(r, &iv, prefix_name)
+		}
+	}
+}
+
 // appends records to our record input queue
 // every now and then, we should pack the input queue into a column, though
 func RunIngestCmdLine() {
@@ -33,7 +55,7 @@ func RunIngestCmdLine() {
 	dec := json.NewDecoder(os.Stdin)
 	count := 0
 	for {
-		var recordmap map[string]interface{}
+		var recordmap Dictionary
 
 		if err := dec.Decode(&recordmap); err != nil {
 			if err == io.EOF {
@@ -47,15 +69,8 @@ func RunIngestCmdLine() {
 
 		r := t.NewRecord()
 
-		for k, v := range recordmap {
-			switch iv := v.(type) {
-			case string:
-				r.AddStrField(k, iv)
-			case float64:
-				r.AddIntField(k, int(iv))
-
-			}
-		}
+		ingest_dictionary(r, &recordmap, "")
+		t.PrintRecord(r)
 
 		count++
 
