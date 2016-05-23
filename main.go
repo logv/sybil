a8569ca12ea2a4b689c3b8ddae6811269d5f5512
okay <okay@chert>
2016-05-23 11:26:02 -0700

    updating the main sybil help output

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import sybil "github.com/logV/sybil/src/lib"
 import cmd "github.com/logV/sybil/src/cmd"
+
+import "fmt"
 import "os"
 import "log"
 import "sort"
@@ -13,10 +15,8 @@ func setupCommands() {
 	CMD_FUNCS["ingest"] = cmd.RunIngestCmdLine
 	CMD_FUNCS["digest"] = cmd.RunDigestCmdLine
 	CMD_FUNCS["session"] = cmd.RunSessionizeCmdLine
-	CMD_FUNCS["expire"] = cmd.RunTrimCmdLine
 	CMD_FUNCS["trim"] = cmd.RunTrimCmdLine
 	CMD_FUNCS["query"] = cmd.RunQueryCmdLine
-	CMD_FUNCS["index"] = cmd.RunIndexCmdLine
 	CMD_FUNCS["rebuild"] = cmd.RunIndexCmdLine
 	CMD_FUNCS["inspect"] = cmd.RunInspectCmdLine
 
@@ -25,9 +25,60 @@ func setupCommands() {
 	}
 }
 
+var USAGE = `sybil: a fast and simple NoSQL column store
+
+Commands: ingest, digest, trim, query, session, rebuild, inspect
+
+Storage Commands:
+
+  ingest: ingest records into the row store
+
+    example: sybil ingest -table TABLE < my_record.json
+    example: sybil ingest -table TABLE -csv < my_records.csv
+
+  digest: collate row store records into column blocks
+
+    example: sybil digest -table TABLE
+
+  trim: trim a table to fit into a set amount of space or time limit
+
+    example: sybil trim -table TABLE -mb 100 -list
+    example: sybil trim -table TABLE -mb 100 -delete
+
+Query Commands:
+
+  query: run aggregation queries on records inside a table
+
+    example: sybil query -table TABLE -info
+    example: sybil query -table TABLE -print -group col1 -int col2 -op hist
+    # reads the row store log (off by default)
+    example: sybil query -table TABLE -read-log -print -group col1 -int col2 -op hist
+
+  [EXPERIMENTAL]
+  session: run a session based query
+    example: sybil session -table ta -time-col time -session userid \
+             -join-table ta_info -join-key userid -join-group browser
+
+
+Emergency Maintenance Commands:
+
+  rebuild: re-create the main table info.db based on the consensus of blocks' info.db
+
+    example: sybil rebuild -table TABLE
+
+  inspect: examine sybil .db files
+
+    example: sybil inspect -file ./db/TABLE/info.db
+    example: sybil inspect -file ./db/TABLE/BLOCK/info.db
+    example: sybil inspect -file ./db/TABLE/BLOCK/str_COL.db
+
+`
+
 func printCommandHelp() {
 	sort.Strings(CMD_KEYS)
-	log.Fatal("Command should be one of: ", CMD_KEYS)
+
+	fmt.Printf(USAGE)
+	log.Fatal()
 }
 
 func main() {
