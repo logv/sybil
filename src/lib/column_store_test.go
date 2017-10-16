62e354425fb487776ce650579ef052fae4e883f7
okay <okay@chalk>
2017-10-16 14:31:30 -0700

    [tests] add big int test for ints over 2**32

--- src/lib/column_store_test.go
+++ src/lib/column_store_test.go
@@ -134,3 +134,70 @@ func TestColumnStoreFileNames(test *testing.T) {
 	}
 
 }
+
+func TestBigIntColumns(test *testing.T) {
+	deleteTestDB()
+
+	var minVal = int64(1 << 50)
+	blockCount := 3
+	addRecordsToTestDB(func(r *Record, index int) {
+		r.AddIntField("id", int64(index))
+		age := int64(rand.Intn(1 << 20))
+		r.AddIntField("time", minVal+age)
+	}, blockCount)
+
+	t := GetTable(testTableName)
+	t.IngestRecords("ingest")
+
+	unloadTestTable()
+	nt := GetTable(testTableName)
+	DELETE_BLOCKS_AFTER_QUERY = false
+	FLAGS.READ_INGESTION_LOG = &TRUE
+
+	nt.LoadTableInfo()
+	nt.LoadRecords(nil)
+
+	if len(nt.RowBlock.RecordList) != CHUNK_SIZE*blockCount {
+		test.Error("Row Store didn't read back right number of records", len(nt.RowBlock.RecordList))
+	}
+
+	if len(nt.BlockList) != 1 {
+		test.Error("Found other records than rowblock")
+	}
+
+	nt.DigestRecords()
+
+	unloadTestTable()
+
+	READ_ROWS_ONLY = false
+	FLAGS.SAMPLES = &TRUE
+	limit := 1000
+	FLAGS.LIMIT = &limit
+	nt = GetTable(testTableName)
+
+	loadSpec := nt.NewLoadSpec()
+	loadSpec.LoadAllColumns = true
+	nt.LoadRecords(&loadSpec)
+
+	count := int32(0)
+	Debug("MIN VALUE BEING CHECKED FOR IS", minVal, "2^32 is", 1<<32)
+	Debug("MIN VAL IS BIGGER?", minVal > 1<<32)
+	for _, b := range nt.BlockList {
+		Debug("VERIFYING BIG INTS IN", b.Name)
+		for _, r := range b.RecordList {
+			v, ok := r.GetIntVal("time")
+			if int64(v) < minVal || !ok {
+				test.Error("BIG INT UNPACKED INCORRECTLY! VAL:", v, "OK?", ok)
+			}
+
+		}
+		count += b.Info.NumRecords
+	}
+
+	if count != int32(blockCount*CHUNK_SIZE) {
+		test.Error("COLUMN STORE RETURNED TOO FEW COLUMNS", count)
+
+	}
+	FLAGS.SAMPLES = &FALSE
+
+}
