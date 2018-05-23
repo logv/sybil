1b9c087245f60bea195b8a38c10c3fc8df6110c9
Travis Cline <travis.cline@gmail.com>
2018-05-23 13:57:31 -0700

    [grpc] add stub comments to satisfy linter

--- src/sybild/server.go
+++ src/sybild/server.go
@@ -7,10 +7,13 @@ import (
 	pb "github.com/logv/sybil/src/sybilpb"
 )
 
+// Server implements SybilServer
 type Server struct{}
 
+// ServerOption describes options to customize Server implementations.
 type ServerOption func(*Server)
 
+// NewServer returns a Server.
 func NewServer(opts ...ServerOption) (*Server, error) {
 	s := &Server{}
 	for _, o := range opts {
@@ -19,22 +22,27 @@ func NewServer(opts ...ServerOption) (*Server, error) {
 	return s, nil
 }
 
+// Serve .
 func (s *Server) Serve() error {
 	return fmt.Errorf("not implemented")
 }
 
+// Ingest .
 func (s *Server) Ingest(context.Context, *pb.IngestRequest) (*pb.IngestResponse, error) {
 	panic("not implemented")
 }
 
+// Query .
 func (s *Server) Query(context.Context, *pb.QueryRequest) (*pb.QueryResponse, error) {
 	panic("not implemented")
 }
 
+// ListTables .
 func (s *Server) ListTables(context.Context, *pb.ListTablesRequest) (*pb.ListTablesResponse, error) {
 	panic("not implemented")
 }
 
+// GetTable .
 func (s *Server) GetTable(context.Context, *pb.GetTableRequest) (*pb.Table, error) {
 	panic("not implemented")
 }
