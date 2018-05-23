a886048a56f2854c464d683e1699de77e23fdae4
Travis Cline <travis.cline@gmail.com>
2018-05-23 14:11:20 -0700

    [grpc] use x/net/context for go<1.9

--- src/sybild/server.go
+++ src/sybild/server.go
@@ -7,13 +7,10 @@ import (
 	pb "github.com/logv/sybil/src/sybilpb"
 )
 
-// Server implements SybilServer
 type Server struct{}
 
-// ServerOption describes options to customize Server implementations.
 type ServerOption func(*Server)
 
-// NewServer returns a Server.
 func NewServer(opts ...ServerOption) (*Server, error) {
 	s := &Server{}
 	for _, o := range opts {
@@ -22,27 +19,22 @@ func NewServer(opts ...ServerOption) (*Server, error) {
 	return s, nil
 }
 
-// Serve .
 func (s *Server) Serve() error {
 	return fmt.Errorf("not implemented")
 }
 
-// Ingest .
 func (s *Server) Ingest(context.Context, *pb.IngestRequest) (*pb.IngestResponse, error) {
 	panic("not implemented")
 }
 
-// Query .
 func (s *Server) Query(context.Context, *pb.QueryRequest) (*pb.QueryResponse, error) {
 	panic("not implemented")
 }
 
-// ListTables .
 func (s *Server) ListTables(context.Context, *pb.ListTablesRequest) (*pb.ListTablesResponse, error) {
 	panic("not implemented")
 }
 
-// GetTable .
 func (s *Server) GetTable(context.Context, *pb.GetTableRequest) (*pb.Table, error) {
 	panic("not implemented")
 }
