f1499a3663db5ca83acf8cd58e4517d4cb1a7e5f
Travis Cline <travis.cline@gmail.com>
2018-06-03 15:45:13 -0700

    [tests] use gzip for json to prevent it showing up in grep

--- src/sybil/decoding_test.go
+++ src/sybil/decoding_test.go
@@ -3,6 +3,7 @@
 package sybil
 
 import (
+	"encoding/base64"
 	"encoding/gob"
 	"encoding/json"
 	"flag"
@@ -42,20 +43,26 @@ func TestDecodeGoldenFiles(t *testing.T) {
 				return
 			}
 
-			goldenJSONPath := fmt.Sprintf("testdata/TestDecodeGoldenFiles/%s.golden.json", tt.name)
+			goldenJSONPath := fmt.Sprintf("testdata/TestDecodeGoldenFiles/%s.golden.json.gz", tt.name)
 			if *flagUpdateGoldenFiles {
-				if err := ioutil.WriteFile(goldenJSONPath, asJSON, 0644); err != nil {
+				b64 := base64.StdEncoding.EncodeToString(asJSON)
+				if err := ioutil.WriteFile(goldenJSONPath, []byte(b64), 0644); err != nil {
 					t.Error(err)
 				}
 				return
 			}
-			goldenJSON, err := ioutil.ReadFile(goldenJSONPath)
+			goldenJSONgz, err := ioutil.ReadFile(goldenJSONPath)
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			goldenJSON, err := base64.StdEncoding.DecodeString(string(goldenJSONgz))
 			if err != nil {
 				t.Error(err)
 				return
 			}
 			if !cmp.Equal(asJSON, goldenJSON) {
-				t.Errorf("%v: golden json differs: %v", tt.name, cmp.Diff(asJSON, goldenJSON, cmp.Transformer("parsejson", func(b []byte) (m map[string]interface{}) {
+				t.Errorf("%v: golden json differs: %v", tt.name, cmp.Diff(goldenJSON, asJSON, cmp.Transformer("parsejson", func(b []byte) (m map[string]interface{}) {
 					if err := json.Unmarshal(b, &m); err != nil {
 						panic(err)
 					}
