9a02420f53cb402a88fa0c25adeb0d5ebbbec2a6
Travis Cline <travis.cline@gmail.com>
2018-06-27 15:33:07 -0700

    [grpc] add basic doc.go

---
+++ src/sybild/doc.go
@@ -0,0 +1,6 @@
+// Package sybild holds the implementation of the sybil grpc service.
+//
+// A sybil server can be started with the `serve` subcommand.
+//
+// Sybil queries are sent to a remote by supplying a value to the `-dial` argument.
+package sybild
